4b2031587bb2df6bad5feed10b16ee10826acb08
Rogerio Marques <rogeriomarquesamancio@gmail.com>
2023-08-01 19:22:57 -0300

    Remove useless

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from 'react';
 import HomeView from './Home.view';
 
 function Home() {
-    useEffect(() => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://www.w3schools.com/xml/plant_catalog.xml');
-        xhr.onload = () => {
-          if (xhr.status === 200) {
-            console.log(xhr.responseText);
-          } else {
-            console.log(xhr.status);
-          }
-        };
-        xhr.send();
-      }, []);
 
     return (
         <HomeView />
